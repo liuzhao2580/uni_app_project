fc9104c53c2c5f4d16c9ae618f368876a0fba26d
liuzhao2580 <1457639270@qq.com>
2020-11-20 21:05:38 +0800

    添加一个新的模板，商品卡片，用来展示商品基本信息（图片、价格...）

---
+++ components/good-card/good-card.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		good-card
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
