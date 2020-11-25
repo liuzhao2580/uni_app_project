2dce4fb4efabb0339c0cdd330958b75f0069f2c0
liuzhao2580 <1457639270@qq.com>
2020-11-25 21:48:30 +0800

    添加 商品信息卡片组件

--- components/good-card/good-card.vue
+++ components/good-card/good-card.vue
@@ -1,22 +1,65 @@
+/** * 商品简介：
+* 只支持三种模式,默认都是 第一种模式 
+* 1、default 正常的图片显示 + 两行文字显示（多余部分省略号）+ 价格（存在旧的价格就显示，不存在就显示新的价格） 
+* 2、middle 中尺寸大小图片显示 + 价格 只适用于 推荐页面秒杀展示 
+* 3、small 小尺寸大小，只显示图片 */
 <template>
-	<view>
-		good-card
+	<view class="goods-card-box" >
+		<!-- 商品图片 -->
+		<view class="goods-image-box">
+			<u-image class="goods-image" :height="height" src="@/static/tabbar/personal-active.png" mode=""></u-image>
+		</view>
+		<!-- 商品简介 -->
+		<view class="goods-details" v-if="size === 'default'">
+			<text>zhesssssssssssssssssssssssssssssssssssssssssssssssssssssss</text>
+		</view>
+		<!-- 商品价格 -->
+		<view class="goods-price">
+			<view class="now-price"><u-icon name="rmb"></u-icon>1999</view>
+			<view class="old-price"><u-icon name="rmb"></u-icon>2888</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
+export default {
+	props:{
+		// 尺寸
+		size: {
+			type: String,
+			default: 'default'
 		},
-		methods: {
-			
+		// 图片高度
+		height: {
+			type: Number,
+			default: 200
 		}
-	}
+	},
+	data() {
+		return {};
+	},
+	methods: {}
+};
 </script>
 
-<style>
-
+<style lang="scss">
+.goods-card-box {
+	height: 300rpx;
+	width: 200rpx;
+	.goods-image-box {
+	}
+	.goods-price {
+		display: flex;
+		align-items: center;
+		.now-price {
+			color: $base-color;
+			margin-right: 10rpx;
+		}
+		.old-price {
+			color: #ccc;
+			text-decoration: line-through;
+			font-size: 16rpx;
+		}
+	}
+}
 </style>
--- pages/tabbar/home/Recommend/components/second-kill.vue
+++ pages/tabbar/home/Recommend/components/second-kill.vue
@@ -11,9 +11,11 @@
 				<u-icon name="arrow-right-double"></u-icon>
 			</view>
 		</view>
-		<view class="sk-box"></view>
+		<view class="sk-box">
+			<good-card size="middle"></good-card>
+		</view>
 		<!-- 更多按钮点击触发模态框 -->
-		<u-modal v-model="moreShow" :content="content"></u-modal>
+		<u-modal v-model="moreShow" content="开发中"></u-modal>
 	</view>
 </template>
 
@@ -24,7 +26,6 @@ export default {
 			countDown: null,
 			howTime: null,
 			moreShow: false,
-			content: '开发中'
 		};
 	},
 	created() {
@@ -35,37 +36,41 @@ export default {
 		timeInter() {
 			// 获取小时
 			const getHours = new Date().getHours();
-			this.howTime = getHours;
 			// 获取当前的时间
 			const getTime = new Date().getTime();
 			// 距离的时间
 			let setTime;
 			// 0-8 点的秒杀
 			if (0 <= getHours && getHours < 8) {
+				this.howTime = 0;
 				setTime = new Date();
 				setTime.setHours(8, 0, 0, 0);
 				this.countDown = (setTime.getTime() - getTime) / 1000;
 			}
 			// 8-12 点的秒杀
 			else if (8 <= getHours && getHours < 12) {
+				this.howTime = 8;
 				setTime = new Date();
 				setTime.setHours(12, 0, 0, 0);
 				this.countDown = (setTime.getTime() - getTime) / 1000;
 			}
 			// 12-16 点的秒杀
 			else if (12 <= getHours && getHours < 16) {
+				this.howTime = 12;
 				setTime = new Date();
 				setTime.setHours(16, 0, 0, 0);
 				this.countDown = (setTime.getTime() - getTime) / 1000;
 			}
 			// 16-20 点的秒杀
 			else if (16 <= getHours && getHours < 20) {
+				this.howTime = 16;
 				setTime = new Date();
 				setTime.setHours(20, 0, 0, 0);
 				this.countDown = (setTime.getTime() - getTime) / 1000;
 			}
 			// 20-0 点的秒杀
 			else if (20 <= getHours && getHours < 24) {
+				this.howTime = 20;
 				setTime = new Date();
 				setTime.setHours(24, 0, 0, 0);
 				this.countDown = (setTime.getTime() - getTime) / 1000;
@@ -82,7 +87,6 @@ export default {
 .second-kill-box {
 	margin: 20rpx 0;
 	background-color: #fff;
-	height: 260rpx;
 	.header-box {
 		display: flex;
 		justify-content: space-between;
