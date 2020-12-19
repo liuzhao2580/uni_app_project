f35dfb6784aca500f96b2c2e4aea3025dc92a96f
liuzhao2580 <1457639270@qq.com>
2020-12-19 20:30:07 +0800

    添加猜你喜欢的模块

---
+++ pages/tabbar/home/Recommend/components/recommend-list.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="recommend-list-box">
+		<!-- 背景图 猜你喜欢 -->
+		<view class="bgc-box">
+			<u-image src="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-minions/5cd31730-41eb-11eb-8a36-ebb87efcf8c0.png" mode="widthFix">
+			</u-image>
+		</view>
+		<!-- 商品列表 -->
+		<view class="">
+			<good-card :goodsCardList='[]'></good-card>
+		</view>
+	</view>
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+.recommend-list-box {
+	margin-top: 10rpx;
+}
+</style>
