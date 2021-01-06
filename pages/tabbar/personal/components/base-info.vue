8b3da0d62341d66488df61e900b336202bcfa67e
liuzhao2580 <1457639270@qq.com>
2021-01-06 20:57:15 +0800

    添加个人中心的静态页面

---
+++ pages/tabbar/personal/components/base-info.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="base-info-box">
+		<view class="base-info">
+			<!-- 头像 -->
+			<view class="user-avatar"><image class="image" src="@/static/tabbar/personal.png"></image></view>
+			<!-- 昵称 -->
+			<text class="user-nickName">小火车况且况且</text>
+			<!-- 个人主页 -->
+			<view class="user-page">
+				<text>个人主页</text>
+				<text class="iconfont icon-arrow-right"></text>
+			</view>
+		</view>
+		<!-- 发布信息 -->
+		
+	</view>
+</template>
+
+<script></script>
+
+<style lang="scss">
+.base-info-box {
+	.base-info {
+		display: flex;
+		align-items: center;
+		.user-avatar {
+			flex: 1.5;
+			.image {
+				width: 120rpx;
+				height: 120rpx;
+				border-radius: 50%;
+			}
+		}
+		.user-nickName {
+			font-size: 32rpx;
+			font-weight: 600;
+			flex: 4;
+		}
+		.user-page {
+			color: #c0c0c0;
+			flex: 2;
+			text-align: right;
+		}
+	}
+}
+</style>
--- pages/tabbar/personal/personal.vue
+++ pages/tabbar/personal/personal.vue
@@ -1,19 +1,26 @@
 <template>
-	<view>
-		
+	<view class="personal-box">
+		<!-- 头部 个人信息 -->
+		<base-info></base-info>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
+import BaseInfo from './components/base-info.vue'
+export default {
+	components:{
+		BaseInfo
+	},
+	data() {
+		return {
+			
+		};
 	}
+}
 </script>
 
 <style lang="scss">
-
+.personal-box {
+	padding: 20rpx;
+}
 </style>
